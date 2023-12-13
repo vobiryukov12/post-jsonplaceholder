dcefabdbd0ddf3db2aae7e8b8a583fcde37bd17a
Vladislav Biryukov <vobiryukov12@gmail.com>
2023-12-13 03:13:06 +0300

    fix: fix a typing error in the Error component

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,4 +1,8 @@
-export function Error({ message }) {
+interface IErrorProps {
+  message: string
+}
+
+export function Error({ message }: IErrorProps) {
   return (
     <div className="flex items-center gap-3 justify-center">
       <div className="text-lg font-medium text-red-600">&#128557;{message}</div>
